5cc8a02c8d012ed95762adb8cc2c194baac66be5
Marcio Vasconcellos <s1vasconcellos@yahoo.com.br>
2020-09-07 14:24:44 -0300

    9. Utilizando a tabela RESERVA_PROGRAMA, escreva uma query que retorne por ordem decrescente o total de reservas por programa.

--- src/main/java/com/marcioprograma/reserva/repository/ReservaProgramaRepository.java
+++ src/main/java/com/marcioprograma/reserva/repository/ReservaProgramaRepository.java
@@ -7,3 +7,6 @@ import com.marcioprograma.reserva.entity.ReservaPrograma;
 
 @Repository
 public interface ReservaProgramaRepository extends CrudRepository<ReservaPrograma, Long> {}
+
+
+/// Select Count(*) from RESERVA_PROGRAMA group by id_programa order by 1 desc; 
