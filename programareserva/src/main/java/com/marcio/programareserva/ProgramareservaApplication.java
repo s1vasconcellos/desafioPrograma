6d48f0ba856f47464033adf06c983eea23d845ab
Marcio Vasconcellos <s1vasconcellos@yahoo.com.br>
2020-09-09 10:30:36 -0300

    Correcao Inicio

---
+++ programareserva/src/main/java/com/marcio/programareserva/ProgramareservaApplication.java
@@ -0,0 +1,13 @@
+package com.marcio.programareserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramareservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramareservaApplication.class, args);
+	}
+
+}
---
+++ programareserva/src/test/java/com/marcio/programareserva/ProgramareservaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcio.programareserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramareservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
