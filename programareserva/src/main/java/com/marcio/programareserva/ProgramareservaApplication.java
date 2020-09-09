704b16163b5c1a28dc93e7e0bd95214912a009eb
Marcio Vasconcellos <s1vasconcellos@yahoo.com.br>
2020-09-09 12:04:21 -0300

    API-Gateway

--- programareserva/src/main/java/com/marcio/programareserva/ProgramareservaApplication.java
+++ programareserva/src/main/java/com/marcio/programareserva/ProgramareservaApplication.java
@@ -2,8 +2,10 @@ package com.marcio.programareserva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProgramareservaApplication {
 
 	public static void main(String[] args) {
